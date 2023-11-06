8940f73f567c4edef8a77e7f01bd89af5e468a52
ConcordDK <148088072+ConcordDK@users.noreply.github.com>
2023-11-06 18:50:16 +0400

    06.11.2023

--- src/main/java/multiplicationTable/MultiplicationTable.java
+++ src/main/java/multiplicationTable/MultiplicationTable.java
@@ -3,9 +3,9 @@ package multiplicationTable;
 public class MultiplicationTable {
     public static void main(String[] args) {
         for (int numberLeft = 1; numberLeft <= 10; numberLeft++) {
-            System.out.println();
+            System.out.println("\n");
             for (int numberUp = 2; numberUp <= 10; numberUp++) {
-                System.out.print(numberLeft * numberUp + " ");
+                System.out.printf("%5d", (numberUp + 1) * (numberLeft + 1));
             }
         }
     }
