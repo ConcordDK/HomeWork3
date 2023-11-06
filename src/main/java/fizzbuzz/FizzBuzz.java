cb78b70afa1a64dd572a571e082e1943c92ea4c3
ConcordDK <148088072+ConcordDK@users.noreply.github.com>
2023-11-06 21:53:21 +0400

    06.11.2023

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -4,14 +4,15 @@ public class FizzBuzz {
     public static void main(String[] args) {
         int number;
         for (number = 1; number <= 100; number++) {
-            if ((number % 3 == 0) && (number % 5 == 0)) ;
-            else if (number % 3 == 0) ;
-            else if (number % 5 == 0) ;
-
-            System.out.println("Buzz");
-            System.out.println("Fizz");
-            System.out.println("FizzBuzz");
-            System.out.println(number);
+            if ((number % 3 == 0) & (number % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            } else if (number % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (number % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(number);
+            }
         }
     }
 }
--- src/main/java/multiplicationTable/MultiplicationTable.java
+++ src/main/java/multiplicationTable/MultiplicationTable.java
@@ -2,11 +2,11 @@ package multiplicationTable;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        for (int numberLeft = 1; numberLeft <= 10; numberLeft++) {
-            System.out.println("\n");
-            for (int numberUp = 2; numberUp <= 10; numberUp++) {
+        for (int numberLeft = 0; numberLeft < 10; numberLeft++) {
+            for (int numberUp = 0; numberUp < 10; numberUp++) {
                 System.out.printf("%5d", (numberUp + 1) * (numberLeft + 1));
             }
+            System.out.println("\n");
         }
     }
 }
