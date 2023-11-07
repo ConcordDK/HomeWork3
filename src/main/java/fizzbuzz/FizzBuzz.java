3f910ce050b224ff37f1c0cc4a5aa8aa354db08b
ConcordDK <148088072+ConcordDK@users.noreply.github.com>
2023-11-07 19:32:10 +0400

    06.11.2023

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -2,16 +2,15 @@ package fizzbuzz;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        int number;
-        for (number = 1; number <= 100; number++) {
+        for (int number = 1; number <= 100; number++) {
             if ((number % 3 == 0) & (number % 5 == 0)) {
-                System.out.println("FizzBuzz");
+                System.out.println(number + " FizzBuzz");
             } else if (number % 3 == 0) {
-                System.out.println("Fizz");
+                System.out.println(number + " Fizz");
             } else if (number % 5 == 0) {
-                System.out.println("Buzz");
+                System.out.println(number + " Buzz");
             } else {
-                System.out.println(number);
+                System.out.println(number + " no Fizz and no Buz");
             }
         }
     }
